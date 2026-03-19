6918be68efe3f24b5f956ecfecfc7f9e79315a4d
ELPC <elshanisayev378@gmail.com>
2026-03-19 13:48:44 +0400

    _on_accept GameInvitationView added gm notification

--- app/discord/views/game_invitation_view.py
+++ app/discord/views/game_invitation_view.py
@@ -1,5 +1,5 @@
 # app/discord/views/game_invitation_view.py
-from disnake import MessageInteraction, ButtonStyle
+from disnake import MessageInteraction, ButtonStyle, Embed, Color, NotFound, Forbidden
 from disnake.ui import Button
 from uuid import UUID
 
@@ -29,6 +29,28 @@ class GameInvitationView(BaseView):
             user = await user_service.get_user_by_discord(inter.author.id)
 
         async with game_service_ctx() as game_service:
+            game = await game_service.get_by_id(game_id)
             await game_service.request_join(game_id, user.id)
 
         await inter.followup.send("✅ Заявка на вступление отправлена", ephemeral=True)
+
+        embed = Embed(
+            title="Новая заявка",
+            description=f"Получена заявка на вступление в игру **{game.name}** от <@{inter.author.id}>.",
+            color=Color.blue()
+        )
+
+        gm = inter.bot.get_user(game.gm_id)
+        if gm is None:
+            try:
+                gm = await inter.bot.fetch_user(game.gm_id)
+            except NotFound:
+                return
+
+        try:
+            await gm.send(embed=embed)
+        except Forbidden:
+            pass
+
+
+
